ce1ad158d32ea02bd35252e2ba12cbfd083cb062
liteng <email@1218609904@qq.com>
2018-08-11 13:12:28 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Button} from 'antd-mobile'
+
+export default function App() {
+  return <Button type='primary'>Hello react!</Button>//Button type='primary'为蓝的按钮
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+入口JS
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {HashRouter, Switch, Route} from 'react-router-dom'
+
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Main from './containers/main/main'
+
+ReactDOM.render((
+  <HashRouter>
+    <Switch>
+      <Route path='/login' component={Login}/>
+      <Route path='/register' component={Register}/>
+      <Route component={Main}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'))
